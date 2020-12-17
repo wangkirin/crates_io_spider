b75ad62028be45e2a04db9a5992af0e674c36513
Wang Kirin <wangqilintech@gmail.com>
2020-12-17 20:10:08 +0800

    add first version of crates_io_spider

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,58 @@
+use crates_io_api::{Error, SyncClient, User};
+
+fn list_top_dependencies() -> Result<(), Error> {
+    // Instantiate the client.
+    let client = SyncClient::new(
+        "my-user-agent (my-contact@domain.com)",
+        std::time::Duration::from_millis(1000),
+    )?;
+    // Retrieve summary data.
+    let summary = client.summary()?;
+    for c in summary.most_downloaded {
+        println!("{}:", c.id);
+        for dep in client.crate_dependencies(&c.id, &c.max_version)? {
+            // Ignore optional dependencies.
+            if !dep.optional {
+                println!("    * {} - {}", dep.id, dep.version_id);
+            }
+        }
+    }
+    Ok(())
+}
+
+fn list_reverse_dependencies(softwareName: &str) -> Result<(), Error> {
+    // Instantiate the client.
+    let client = SyncClient::new(
+        "my-user-agent (my-contact@domain.com)",
+        std::time::Duration::from_millis(1000),
+    )?;
+    // Retrieve summary data.
+    let reverse_dep = client.crate_reverse_dependencies(softwareName)?;
+    println!(
+        "Number of reverse_dependencies {}:",
+        reverse_dep.dependencies.len()
+    );
+
+    for rd in reverse_dep.dependencies {
+        let v = rd.crate_version;
+        // let publisher = match v.published_by {
+        //     Some(p) => p,
+        //     None => crates_io_api::User {
+        //         name: String::from("NONE"),
+        //         ...
+        //     },
+        // };
+        println!(
+            // "[{}]  - [{}]  -[{:#?}]]  ",
+            "[{}]  - [{}]   ",
+            v.crate_name,
+            v.downloads,
+            // v.published_by.unwrap().name,
+        );
+    }
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    let l1 = list_reverse_dependencies("rayon").unwrap();
 }
